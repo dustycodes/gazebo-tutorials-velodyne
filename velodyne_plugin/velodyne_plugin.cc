747ec3f11a0fde7b02b9ad57f9d51176bba7c553
dusty <d.argyle@sarcos.com>
2017-12-01 17:13:12 -0700

    Finished with ROS API portion of tutorial

--- velodyne_plugin/velodyne_plugin.cc
+++ velodyne_plugin/velodyne_plugin.cc
@@ -1,10 +1,18 @@
 #ifndef _VELODYNE_PLUGIN_HH_
 #define _VELODYNE_PLUGIN_HH_
 
+//  Gazebo api
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
 #include <gazebo/transport/transport.hh>
 #include <gazebo/msgs/msgs.hh>
+
+// ROS
+#include <thread>
+#include "ros/ros.h"
+#include "ros/callback_queue.h"
+#include "ros/subscribe_options.h"
+#include "std_msgs/Float32.h"
 
 namespace gazebo
 {
@@ -71,6 +79,32 @@ public:
         // Subscribe to the topic, and register a callback
         this->sub = this->node->Subscribe(topicName,
             &VelodynePlugin::OnMsg, this);
+
+        // Initialize ros, if it has not already bee initialized.
+        if (!ros::isInitialized())
+        {
+            int argc    = 0;
+            char** argv = NULL;
+            ros::init(argc, argv, "gazebo_client",
+              ros::init_options::NoSigintHandler);
+        }
+
+        // Create our ROS node. This acts in a similar manner to
+        // the Gazebo node
+        this->rosNode.reset(new ros::NodeHandle("gazebo_client"));
+
+        // Create a named topic, and subscribe to it.
+        ros::SubscribeOptions so =
+          ros::SubscribeOptions::create<std_msgs::Float32>(
+            "/" + this->model->GetName() + "/vel_cmd",
+            1,
+            boost::bind(&VelodynePlugin::OnRosMsg, this, _1),
+            ros::VoidPtr(), &this->rosQueue);
+        this->rosSub = this->rosNode->subscribe(so);
+
+        // Spin up the queue helper thread.
+        this->rosQueueThread =
+          std::thread(std::bind(&VelodynePlugin::QueueThread, this));
     } // Load
 
     /// \brief Set the velocity of the Velodyne
@@ -82,6 +116,14 @@ public:
             this->joint->GetScopedName(), _vel);
     }
 
+    /// \brief Handle an incoming message from ROS
+    /// \param[in] _msg A float value that is used to set the velocity
+    /// of the Velodyne.
+    void OnRosMsg(const std_msgs::Float32ConstPtr &_msg)
+    {
+        this->SetVelocity(_msg->data);
+    }
+
 private:
     /// \brief Handle incoming message
     /// \param[in] _msg Repurpose a vector3 message. This function will
@@ -89,6 +131,17 @@ private:
     void OnMsg(ConstVector3dPtr &_msg)
     {
         this->SetVelocity(_msg->x());
+    }
+
+    /// \brief ROS helper function that processes messages
+    void QueueThread()
+    {
+        static const double timeout = 0.01;
+
+        while (this->rosNode->ok())
+        {
+            this->rosQueue.callAvailable(ros::WallDuration(timeout));
+        }
     }
 
 private:
@@ -106,6 +159,18 @@ private:
 
     /// \brief A subscriber to a named topic.
     transport::SubscriberPtr sub;
+
+    /// \brief A node use for ROS transport
+    std::unique_ptr<ros::NodeHandle> rosNode;
+
+    /// \brief A ROS subscriber
+    ros::Subscriber rosSub;
+
+    /// \brief A ROS callbackqueue that helps process messages
+    ros::CallbackQueue rosQueue;
+
+    /// \brief A thread the keeps running the rosQueue
+    std::thread rosQueueThread;
 };
 
 // Tell Gazebo about this plugin, so that Gazebo can call Load on this plugin.
